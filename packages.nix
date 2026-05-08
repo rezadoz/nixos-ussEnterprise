abdbdd727fe9fe706942ac411d548ce1223a7b25
rezadoz <rezadoz@gmail.com>
2026-05-08 19:08:16 -0400

    Initial NixOS configuration commit

---
+++ packages.nix
@@ -0,0 +1,86 @@
+{ config, pkgs, ... }:
+
+{
+  environment.systemPackages = with pkgs; [
+    # applications
+    btop
+    catnip
+    feh
+    fish
+    ffmpeg
+    geeqie
+    gimp gimpPlugins.gmic
+    libreoffice-qt6
+    mpv
+    qbittorrent
+    qemu_full
+    quickemu # qemu wrapper
+    retroarch-full
+    steam protonup-qt
+    tlrc # tldr
+    virtualbox
+    wineWow64Packages.waylandFull
+    winetricks
+    yazi
+    zathura
+    #zapzap # whatapp
+    zsh zsh-powerlevel10k
+
+    # utilities
+    _7zz-rar unrar
+    bat
+    fastfetch
+    file
+    hunspellDicts.en-us-large
+    imagemagick
+    lsd
+    ripgrep
+    rsclock
+    nix-output-monitor
+    nmap
+    qdirstat
+    weather
+    unzip
+    yt-dlp
+    wavemon
+    zip
+
+    # dev pkgs
+    autoconf
+    automake
+    binutils
+    clang
+    cmake
+    curl
+    gcc
+    gdb
+    git
+    gnumake
+    libtool
+    llvm
+    meson
+    ninja
+    openssl
+    patch
+    pkg-config
+    python3
+    rustup
+    stdenv.cc
+    wget
+  ];
+
+  fonts.packages = with pkgs; [
+    nerd-fonts.agave
+    nerd-fonts.fira-code
+    nerd-fonts.hack
+    nerd-fonts.iosevka
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.meslo-lg
+    nerd-fonts.victor-mono
+    nerd-fonts.zed-mono
+    cozette dina-font
+    liberation_ttf
+    noto-fonts
+    noto-fonts-color-emoji
+  ];
+}
